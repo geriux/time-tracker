7f5b68a698bafab9b97fc5be7d128481819a935a
Gerardo Pacheco <gerardo.sicart@gmail.com>
2025-04-19 17:32:01 +0200

    Adds NativeWind config

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+const { hairlineWidth } = require("nativewind/theme");
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
+  presets: [require("nativewind/preset")],
+  theme: {},
+  plugins: [],
+};
