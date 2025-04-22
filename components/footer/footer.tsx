284ef065016b6ce2a02f5722a44ae4b863096f84
Gerardo Pacheco <gerardo.sicart@gmail.com>
2025-04-22 16:24:19 +0200

    Update Footer component to include link to the Stats screen

--- components/footer/footer.tsx
+++ components/footer/footer.tsx
@@ -1,20 +1,23 @@
+import { Link } from "expo-router";
 import { Image, View, TouchableOpacity } from "react-native";
 
 const Footer = () => {
   return (
     <View className="flex-row justify-between px-8">
-      <TouchableOpacity activeOpacity={0.6}>
-        <View
-          className="w-[60px] h-[60px] rounded-[30px] 
+      <Link href="/stats" asChild>
+        <TouchableOpacity activeOpacity={0.6}>
+          <View
+            className="w-[60px] h-[60px] rounded-[30px] 
       border border-primary justify-center items-center"
-        >
-          <Image
-            source={require("@/assets/images/stats.png")}
-            className="w-[24px] h-[24px]"
-            resizeMode="contain"
-          />
-        </View>
-      </TouchableOpacity>
+          >
+            <Image
+              source={require("@/assets/images/stats.png")}
+              className="w-[24px] h-[24px]"
+              resizeMode="contain"
+            />
+          </View>
+        </TouchableOpacity>
+      </Link>
     </View>
   );
 };
