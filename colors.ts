266bcaf84dc00931c4b6f88b81d84bbc28ba07f6
Gerardo Pacheco <gerardo.sicart@gmail.com>
2025-04-24 17:01:56 +0200

    ActivityButton - Update shadow style

--- colors.ts
+++ colors.ts
@@ -4,6 +4,7 @@ const colors = {
   tertiary: "#E7C9A1",
   neutral: "#FEFAE0",
   accent: "#FFFFFF",
+  shadow: "rgba(212, 163, 115, 0.3)",
 };
 
 export default colors;
--- components/activities-list/activity-button.tsx
+++ components/activities-list/activity-button.tsx
@@ -3,16 +3,7 @@ import GradientBackground from "@/components/gradient-background";
 import colors from "@/colors";
 import { Activity } from "@/common/activities";
 
-const shadowProps = {
-  shadowColor: colors.primary,
-  shadowOffset: {
-    width: 0,
-    height: 4,
-  },
-  shadowOpacity: 0.3,
-  shadowRadius: 0.8,
-  elavation: 2,
-};
+const boxShadow = `0 4 2 0 ${colors.shadow}`;
 
 function ActivityButton({
   activity,
@@ -29,30 +20,28 @@ function ActivityButton({
 
   return (
     <TouchableOpacity onPress={onPressActivity} activeOpacity={0.8}>
-      <View style={shadowProps}>
-        <View
-          className="bg-neutral mr-[15px] rounded-[19px]
+      <View
+        className="bg-neutral mr-[15px] rounded-[19px]
      justify-center items-center  overflow-hidden px-4"
-          style={{ width: size, height: size }}
-        >
-          <GradientBackground
-            gradientColors={[colors.neutral, colors.secondary]}
-            locations={[0, 0.96]}
-            height={size}
-          />
+        style={{ width: size, height: size, boxShadow }}
+      >
+        <GradientBackground
+          gradientColors={[colors.neutral, colors.secondary]}
+          locations={[0, 0.96]}
+          height={size}
+        />
 
-          <View className="w-[75px] h-[75px] mb-4">
-            <Image
-              source={activity.icon}
-              className="w-full h-full"
-              resizeMode="contain"
-            />
-          </View>
-          <View className="bg-primary w-[100%] h-[36px] rounded-[16px] items-center justify-center">
-            <Text className="text-neutral font-condensed text-xl text-center pb-[0.5px]">
-              {activity.name}
-            </Text>
-          </View>
+        <View className="w-[75px] h-[75px] mb-4">
+          <Image
+            source={activity.icon}
+            className="w-full h-full"
+            resizeMode="contain"
+          />
+        </View>
+        <View className="bg-primary w-[100%] h-[36px] rounded-[16px] items-center justify-center">
+          <Text className="text-neutral font-condensed text-xl text-center pb-[0.5px]">
+            {activity.name}
+          </Text>
         </View>
       </View>
     </TouchableOpacity>
