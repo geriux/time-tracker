3ae987e3f3ce7c62e5b7aec6dea622a9bda43214
Gerardo Pacheco <gerardo.sicart@gmail.com>
2025-04-24 09:48:43 +0200

    Update Footer component with logout button

--- components/footer/footer.tsx
+++ components/footer/footer.tsx
@@ -1,7 +1,23 @@
-import { Link } from "expo-router";
-import { Image, View, TouchableOpacity } from "react-native";
+import { router, Link } from "expo-router";
+import { Alert, Image, View, TouchableOpacity } from "react-native";
+import { useAuth } from "@/common/auth";
 
 const Footer = () => {
+  const { signOut } = useAuth() ?? {};
+
+  const onLogOut = () => {
+    if (signOut) {
+      signOut({
+        onLogout: () => {
+          router.replace("/");
+        },
+        onLogoutError: (message: string) => {
+          Alert.alert("Logout error:", message);
+        },
+      });
+    }
+  };
+
   return (
     <View className="flex-row justify-between px-8">
       <Link href="/stats" asChild>
@@ -18,6 +34,19 @@ const Footer = () => {
           </View>
         </TouchableOpacity>
       </Link>
+
+      <TouchableOpacity activeOpacity={0.6} onPress={onLogOut}>
+        <View
+          className="w-[60px] h-[60px] rounded-[30px] 
+      border border-primary justify-center items-center ml-3"
+        >
+          <Image
+            source={require("@/assets/images/logout.png")}
+            className="w-[24px] h-[24px]"
+            resizeMode="contain"
+          />
+        </View>
+      </TouchableOpacity>
     </View>
   );
 };
